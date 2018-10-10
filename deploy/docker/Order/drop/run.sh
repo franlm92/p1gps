ee09cc22b628f6f9abaa02ffebaa69e0d4fb81be
FRANCISCO LOPEZ MUNOZ <Fco.Lopez3@alu.uclm.es>
2018-10-10 13:57:53 +0200

    Corregimos error en el drop/run con el while

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -2,7 +2,7 @@
 #no lanzar el .jar hasta que no esté accesible el servicio de MongoDB
 #el host ‘mongo’ tendrá una IP otorgada al lanzar la pila de #servicios, y le damos
 #el apodo ‘mongo’, utilizado aquí y en el #archivo application.properties
-While ! curl http://mongo:27017/
+while ! curl http://mongo:27017/
 do
   echo "$(date) - still trying"
   sleep 1
